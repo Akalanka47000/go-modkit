93fba05e1af07ba9705044bea395218c87c5725b
Akalanka Perera <akalankaperera128@gmail.com>
2025-05-03 12:51:47 +0530

    docs(routing): updated versionable prefix description

--- routing/main.go
+++ routing/main.go
@@ -5,7 +5,10 @@ import (
 )
 
 // Returns a function that formats a given module name with a version number. Useful for generating versioned module prefixes in REST APIs.
-// Example usage: routing.VersionablePrefix("analysis")(1) returns "/v1/analysis"
+//
+// Example usage:
+//
+// routing.VersionablePrefix("analysis")(1) // returns "/v1/analysis"
 func VersionablePrefix(moduleName string) func(int) string {
 	return func(version int) string {
 		return fmt.Sprintf("/v%d/%s", version, moduleName)
